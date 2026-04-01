5a90a42c7c2302d8dcfeebddc2aedfc30013ba95
pavelgagachev1-svg <pavelgagachev1@gmail.com>
2026-04-01 20:37:54 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,36 +1,21 @@
-var pavel_form_handler = document.getElementById('pavel-form-logic');
+var p_form = document.getElementById('pavel_msg_form');
 
-function check_inputs(name, mail, msg) {
-    if (name.length < 3) {
-        alert("Молам внесете го вашето име.");
-        return false;
-    }
-    if (mail.indexOf('@') == -1) {
-        alert("Внесете важечка е-маил адреса.");
-        return false;
-    }
-    if (msg.length < 5) {
-        alert("Вашата порака е премногу кратка.");
-        return false;
-    }
-    return true;
-}
-
-pavel_form_handler.onsubmit = function(event) {
+p_form.onsubmit = function(event) {
     event.preventDefault();
     
-    var sender_name = document.getElementById('v-name').value;
-    var sender_mail = document.getElementById('v-email').value;
-    var sender_msg = document.getElementById('v-msg').value;
+    var n = document.getElementById('user_name').value;
+    var m = document.getElementById('user_email').value;
 
-    var valid = check_inputs(sender_name, sender_mail, sender_msg);
-
-    if (valid == true) {
-        alert("Ви благодариме " + sender_name + "! Вашата порака е испратена.");
-        pavel_form_handler.reset();
+    if (n.length < 3) {
+        alert("Молам внесете цело име.");
+    } else if (m.indexOf('@') == -1) {
+        alert("Внесете точен е-маил.");
+    } else {
+        alert("Благодарам " + n + "! Пораката е примена.");
+        p_form.reset();
     }
 };
 
-document.querySelector('.header-contact-btn').onclick = function() {
+document.getElementById('contact_jump').onclick = function() {
     document.getElementById('contact').scrollIntoView({ behavior: 'smooth' });
 };
